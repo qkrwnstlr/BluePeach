12fb12513bb38b7e06d77de8c5db45628bb312cc
YeaChan05 <qkenrdl05@gmail.com>
2022-01-12 11:21:05 +0900

    start

---
+++ ping-pong/Assets/Swipe.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swipe : MonoBehaviour
+{
+   private bool tap, swipeLeft, swipeRight,swipeUp, swipeDown;
+   private Vector2 startTouch,swipeDelta;
+    private void Update()
+    {
+        tap=swipeLeft=swipeRight=swipeUp=swipeDown=false;
+
+        #region Moblie Inputs
+        if(Input.touches.Length>0)
+        {
+            if(Input.touches[0].phase==TouchPhase.Began)
+            {
+                tap=true;
+                startTouch=Input.touches[0].position;
+            }
+        }
+        else if(Input.touches[0].phase==TouchPhase.Ended || Input.touches[0].phase==TouchPhase.Canceled)
+        {
+            Reset();
+        }
+        #endregion
+    }
+
+    private void Reset()
+    {
+        startTouch =swipeDelta =Vector2.zero;
+    }
+   public Vector2 SwipeDelta { get { return swipeDelta; }}
+   public bool SwipeLeft { get { return swipeLeft; }}
+   public bool SwipeRight { get { return swipeRight; }}
+   public bool SwipeUp { get { return swipeUp; }}
+   public bool SwipeDown { get { return swipeDown; }}
+
+}
