9d438e8667581c31eeb9070c6b0fdfe36d96ad09
YeaChan05 <qkenrdl05@gmail.com>
2022-01-12 16:01:25 +0900

    start

--- ping-pong/Assets/Swipe.cs
+++ ping-pong/Assets/Swipe.cs
@@ -5,25 +5,31 @@ using UnityEngine;
 public class Swipe : MonoBehaviour
 {
    private bool tap, swipeLeft, swipeRight,swipeUp, swipeDown;
+   private bool isDragging= false; 
    private Vector2 startTouch,swipeDelta;
     private void Update()
     {
         tap=swipeLeft=swipeRight=swipeUp=swipeDown=false;
 
-        #region Moblie Inputs
-        if(Input.touches.Length>0)
+        // #region Moblie Inputs
+        // if(Input.touches.Length>0)
+        // {
+        //     if(Input.touches[0].phase==TouchPhase.Began)
+        //     {
+        //         tap=true;
+        //         startTouch=Input.touches[0].position;
+        //     }
+        // }
+        // else if(Input.touches[0].phase==TouchPhase.Ended || Input.touches[0].phase==TouchPhase.Canceled)
+        // {
+        //     Reset();
+        // }
+        // #endregion
+        swipeDelta=Vector2.zero;
+        if(isDragging)
         {
-            if(Input.touches[0].phase==TouchPhase.Began)
-            {
-                tap=true;
-                startTouch=Input.touches[0].position;
-            }
+            
         }
-        else if(Input.touches[0].phase==TouchPhase.Ended || Input.touches[0].phase==TouchPhase.Canceled)
-        {
-            Reset();
-        }
-        #endregion
     }
 
     private void Reset()
